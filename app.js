bc48fe5fa33cbd589474a9c9f2095f17906b1c48
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-22 15:05:11 -0400

    GET and POST requests complete

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const pgp = require('pg-promise')();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const config = {
   host: 'localhost',
@@ -17,5 +21,30 @@ app.get('/api/restaurants', (req, res) => {
     res.json(results);
   });
 }); 
+
+app.get('/api/restaurants/:id', (req, res) => {
+  db.oneOrNone('SELECT * FROM restaurant WHERE restaurant.id = $1', req.params.id)
+    .then((result) => {
+      if (result) {
+        res.json(result);
+    } else {
+      res.status(404).json({});
+    }
+  })
+    .catch((e) => {
+      res.status(500).json({
+        error: 'Database Error',
+      });
+    });
+});
+
+app.post('/api/restaurants', (req, res) => {
+  // const { name, distance, stars, category, fav, takeout, visited } = req.body;
+  // console.log(req.body);
+  db.one('INSERT INTO restaurant VALUES (DEFAULT, ${name}, ${distance}, ${stars}, ${category}, ${fav}, ${takeout}, ${visited}) RETURNING *', req.body)
+  .then((result) => {
+    res.status(201).json(result);
+  })
+});
 
 app.listen(PORT, () => console.log(`Running: http://localhose:${PORT}`));
